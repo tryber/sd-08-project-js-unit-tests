74339923b4722382fb88449293c284f0039ce526
Paulo Simões <paulohbsimoes@gmail.com>
2020-11-26 15:54:01 -0300

    Requisito 4 concluido

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,15 +23,26 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
+const onlyTwoDecimalPlaces = (num) => {
+  return Number(num.toFixed(2));
+}
+
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.keys(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(onlyTwoDecimalPlaces(circle(2).circumference), 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(onlyTwoDecimalPlaces(circle(3).area), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.strictEqual(circle(3).radius, 3);
+    assert.strictEqual(onlyTwoDecimalPlaces(circle(3).area), 28.26);
+    assert.strictEqual(onlyTwoDecimalPlaces(circle(3).circumference), 18.84);
   });
 });
