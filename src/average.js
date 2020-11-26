d478b08db5ea844345be331e07a475a85b0c37e4
Maurice Alves <maurice.estevam@gmail.com>
2020-11-26 17:14:14 -0300

    Requisito - 1.

--- src/average.js
+++ src/average.js
@@ -12,6 +12,16 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
 
+  let sum = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    sum += array[i];
+  }
+  const media = sum / array.length;
+  return media;
+};
 module.exports = average;
