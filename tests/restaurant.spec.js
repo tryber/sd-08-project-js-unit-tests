21f42c4a65fbe5697a1ab959b107960a77048f1c
Silvio Dayube <scarige@gmail.com>
2020-11-26 18:09:39 -0300

    Req 9.2.5 - Implements test 5

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -90,6 +90,7 @@ describe('#createMenu', () => {
     // objetoRetornado.order("coxinha");
     // objetoRetornado.comsuption // Retorno: ["coxinha"]
     // ```
+    assert.deepStrictEqual(createMenu(testObj).order('coxinha').consumption, ['coxinha']);
     // Agora faça o PASSO 3 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 6: Verifique que as três orders seguintes, de bebidas e comidas mescladas, somam três itens no array `objetoRetornado.consumption` conforme os itens pedidos.
