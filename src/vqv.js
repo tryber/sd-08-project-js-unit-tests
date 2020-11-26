b6c675f3b3d2d3a4e9747d73676abac5e9c89532
Ruben Albertassi <albertassi88@gmail.com>
2020-11-26 15:46:04 -0300

    Criacao da funcao vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === "" || idade === "") {
+    return nome; 
+  } else {
+    return `Oi, meu nome é ${nome}!\n
+    Tenho ${idade} anos,\n
+    trabalho na Trybe e mando muito em programação!\n
+    #VQV!` 
+  }
+};
 
 module.exports = vqv;
