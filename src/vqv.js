12778889d51bb03e3285a9d8c24a1c8a22d04f29
liisaambrosen <liisa.ambrosen@gmail.com>
2020-11-26 17:30:19 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,10 +17,7 @@
 */
 
 function vqv(nome, idade) {
-  return `Oi, meu nome é ${nome}!
-Tenho ${idade} anos,
-trabalho na Trybe e mando muito em programação!
-#VQV!`;
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 }
 
 
