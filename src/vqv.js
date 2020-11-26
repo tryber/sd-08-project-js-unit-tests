f5f62931ef23b44b821370af5228c3b71d85e55e
Pedro Amaral <amaralph1995@gmail.com>
2020-11-26 18:31:34 -0300

    Requisito 4 - arquivo circle.js

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,12 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined
   }
-  return       'Oi, meu nome é ' + nome + '!\n' +
-  'Tenho ' + idade + ' anos,\n' +
-  'trabalho na Trybe e mando muito em programação!\n' +
-  '#VQV!'
+  return (
+    `Oi, meu nome é ${nome}!
+    Tenho  ${idade} anos,
+    trabalho na Trybe e mando muito em programação!
+    #VQV!`)
 };
 
+console.log(vqv('amaral', 25))
 module.exports = vqv;
