2679d5d5a1be8e5eea0348ea2df05187b8d588b9
Victor Breno <victor.brns2@gmail.com>
2020-11-26 19:09:19 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const obj = {};
+  obj.name = nome;
+  obj.feedback = () => 'Eita pessoa boa!';
+  return obj;
+};
 
 module.exports = createStudent;
