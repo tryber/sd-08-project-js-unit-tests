531e4a95cbf112f288ea8ead243a19552d048860
liisaambrosen <liisa.ambrosen@gmail.com>
2020-11-26 17:05:42 -0300

    resolvendo problema req 3

--- src/vqv.js
+++ src/vqv.js
@@ -19,7 +19,7 @@
 function vqv(nome, idade) {
   return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
-  Trabalho na Trybe e mando muito em programação!
+  trabalho na Trybe e mando muito em programação!
   #VQV!`;
 }
 
