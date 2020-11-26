470e17ff51b50041eb0207662bceedc7a6c1f9e1
Ruben Albertassi <albertassi88@gmail.com>
2020-11-26 15:55:43 -0300

    Alteracao na funcao vqv

--- src/vqv.js
+++ src/vqv.js
@@ -23,6 +23,6 @@ const vqv = (nome, idade) => {
   return `Oi, meu nome é ${nome}!\n
   Tenho ${idade} anos,\n
   trabalho na Trybe e mando muito em programação!\n
-  #VQV!`;  
+  #VQV!`;
 };
 module.exports = vqv;
