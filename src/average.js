d475b46af99ad26c0ab09063c3c435d3e4914344
Douglas Ferreira <dodofp.df@gmail.com>
2020-11-26 19:00:41 -0300

    Tentativa de push corrigindo erros

--- src/average.js
+++ src/average.js
@@ -14,9 +14,12 @@
 
 const average = (array) => {
   let sum = 0;
+  if (array.length <= 0) {
+    return undefined;
+  }
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof array[index] !== 'number' || typeof array[index] === []) {
-      throw Error(undefined);
+    if (typeof array[index] !== 'number') {
+      return undefined;
     }
     sum += array[index];
   }
