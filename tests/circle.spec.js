d03021da38a112e1b00e78158bd98522ebe14f91
josebejohann <josebernardojohann@gmail.com>
2020-11-26 15:36:50 -0300

    Fixes constant name

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -26,20 +26,20 @@ const circle = require('../src/circle');
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // Teste se circle retorna um objeto.
-    const circle = circle(3);
-    assert.strictEqual(typeof circle, 'object');
+    const object = circle(3);
+    assert.strictEqual(typeof object, 'object');
 
     // Teste se o objeto retornado tem 3 entradas.
-    assert.deepStrictEqual(circle, Object.keys(circle).length);
+    assert.deepStrictEqual(object, Object.keys(object).length);
 
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
 
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.deepStrictEqual(circle(2), circle.circumference);
+    assert.deepStrictEqual(circle(2), object.circumference);
 
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.deepStrictEqual(circle(2), circle.area);
+    assert.deepStrictEqual(circle(2), object.area);
 
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3), {
