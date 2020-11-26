f4c62a3868117622a4eb5b752c13da2f39d0863d
josebejohann <josebernardojohann@gmail.com>
2020-11-26 16:49:24 -0300

    Fixes codeclimate errors

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
-  return {
-    name,
-    feedback: () => 'Eita pessoa boa!',
-  };
-};
+const createStudent = (name) => { return { name, feedback: () => 'Eita pessoa boa!' }; };
 
 module.exports = createStudent;
