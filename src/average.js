511e88f70dec2394041eaa6be2ee47951f8ab066
Leandro Lachini <leandrolachini@gmail.com>
2020-11-26 18:53:27 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sumArray = 0;
+  if (array.length === 0){
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if(typeof(array[index]) !== 'number'){
+      return undefined;
+    }
+    sumArray += array[index];
+  }
+  return (Math.round(sumArray/array.length));
+};
+
+const array = [1, 2, 3, '4', 5];
+average(array);
 
 module.exports = average;
