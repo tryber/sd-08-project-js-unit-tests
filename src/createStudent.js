1ceab64e77b8c0ec2fbfabff09421036b907b259
Hugo Andrade Braga <hugobrr@gmail.com>
2020-11-26 18:32:37 -0300

    5 requisito passando no local e cc passando no local - 44%

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const resultado = {
+    name: nome,
+    feedback: () => {
+      return 'Eita pessoa boa!';
+    },
+  };
+  return resultado;
+};
 
 module.exports = createStudent;
