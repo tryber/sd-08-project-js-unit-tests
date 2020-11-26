2408ec29eb515c73273cb4aa9d332632034dfc25
Ruben Albertassi <albertassi88@gmail.com>
2020-11-26 20:33:16 -0300

    Alteracao na funcao objCalculator e createStudent

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,9 +20,9 @@
 */
 
 const calculator = {
-  add: ( num1, num2 ) => num1 + num2, 
-  mult: ( num1, num2 ) => num1 * num2,
-  div: ( num1, num2 ) => Math.floor(num1 / num2),
-  sub: ( num1, num2 ) => num1 - num2,
+  add: (num1, num2) => num1 + num2, 
+  mult: (num1, num2) => num1 * num2,
+  div: (num1, num2) => Math.floor(num1 / num2),
+  sub: (num1, num2) => num1 - num2,
 };
 module.exports = calculator;
