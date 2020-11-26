25b0e33477a400561f02076374f3a63382522fae
fabio-infotech <fabio35547@gmail.com>
2020-11-26 15:50:59 -0300

    Requisito 1 - Ajuste Code Climate 1

--- src/average.js
+++ src/average.js
@@ -18,12 +18,12 @@ const average = (array) => {
   }
   let sum = 0;
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof(array[index]) !== 'number') {
+    if (typeof (array[index]) !== 'number') {
       return undefined;
     }
     sum += array[index];
   }
   return Math.round(sum / array.length);
-}
+};
 
 module.exports = average;
