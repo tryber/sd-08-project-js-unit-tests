beca0f736491080680ee61472193c8dc3366288b
Tiago Lunardi <tiagaoalb@gmail.com>
2020-11-26 18:35:50 -0300

    added forth requirement

--- src/vqv.js
+++ src/vqv.js
@@ -20,8 +20,8 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  const result = 
-  `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`
+  const result =
+  `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 
   return result;
 };
