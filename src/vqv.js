1ba042b9a7a392a02fd5f7fe7f378012587fd707
Nícolas Tozato <ntozato9@gmail.com>
2020-11-26 20:50:24 -0300

    refatora requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -20,7 +20,8 @@ const vqv = (nome, idade) => {
   if (nome === undefined && idade === undefined) {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  const result = `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  return result;
 };
 
 module.exports = vqv;
