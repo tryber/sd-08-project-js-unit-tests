b4627b23f1a08b8d821f5cf6ae0a72ba21876428
Rosiele David <rosieleserpa@hotmail.com>
2020-11-26 16:43:07 -0300

    requisito 3: corrigindo função vqv

--- src/vqv.js
+++ src/vqv.js
@@ -17,8 +17,11 @@
 */
 
 const vqv = (nome, idade) => {
-  if (!nome || !idade) return undefined;
-  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`
+  if (nome === undefined || idade === undefined) return undefined;
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
