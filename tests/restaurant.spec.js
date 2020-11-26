b7a9ec421e440099b314e9b3dca87518b94ef40b
Silvio Dayube <scarige@gmail.com>
2020-11-26 20:18:03 -0300

    Req 9.3.7 - Adds test 7

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -118,6 +118,11 @@ describe('#createMenu', () => {
     // objetoRetornado.order('coxinha');
     // objetoRetornado.comsuption // Retorno: ['coxinha', 'agua', 'coxinha']
     // ```
+    Object.assign(restaurant, createMenu(testObj));
+    restaurant.order('coxinha');
+    restaurant.order('agua');
+    restaurant.order('coxinha');
+    assert.deepStrictEqual(restaurant.consumption, ['coxinha', 'agua', 'coxinha']);
     // Agora faça o TESTE 8 deste arquivo.
     // --------------------------------------------------------------------------------------
     // TESTE 8: Verifique que, ao chamar `objetoRetornado.pay()`, retorna-se a soma dos preços de tudo que foi pedido, conforme registrado em `objetoRetornado.consumption`
