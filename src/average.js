13d653469c6ba9d1ed134d449f4203abdab6f6cc
Douglas Fernando <doug.fernando95@gmail.com>
2020-11-26 19:02:35 -0300

    Req 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (array) => {};
+const average = (array) => {
+  let sum = null;
+  if (array.length > 0) {
+    for (let index = 0; index < array.length; index += 1) {
+      if (typeof array[index] !== 'number') {
+        return undefined;
+      } else {
+        sum += array[index];
+      }
+    }
+  }
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  return Math.round(sum / array.length);
+};
 
 module.exports = average;
