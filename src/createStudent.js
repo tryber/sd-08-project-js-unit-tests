a1e73fb58ff885aeea8d9b9427a27707256eb29e
Ronald Jr <rpintojunior@gmail.com>
2020-11-26 18:21:54 -0300

    Corrigi CodeClimate, deixando em uma linha só

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,10 +15,5 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
-  return name = {
-    name: name,
-    feedback: () => ('Eita pessoa boa!')
-  }
-};
+const createStudent = name => ({ name, feedback: () => 'Eita pessoa boa!' });
 module.exports = createStudent;
