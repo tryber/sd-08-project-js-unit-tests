d1b05ceed44bc6899434ad5f85f711048d8ac8f6
Mateus Coury <mateuscoury@gmail.com>
2020-11-26 19:32:22 -0300

    commit do amor 2

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -51,9 +51,6 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.strictEqual(createMenu({ ze: 4, zezinho: 5 }), {
-      fetchMenu: { ze: 4, zezinho: 5 },
-    });
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
     // ```
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
