a5b2728ec0a3d41bc9b9b628058245f93950725f
Lucas Nascimento <lucasmvnascimento@gmail.com>
2020-11-26 17:47:33 -0300

    add req 4 5 6

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -26,6 +26,7 @@ const circle = require('../src/circle');
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
+    const circle3 = {radius: 3, area: 28.259999999999998, circumference: 18.84};
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof (circle(1)), 'object');
     // Teste se o objeto retornado tem 3 entradas.
@@ -35,7 +36,8 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(Object.values(circle(2))[2], 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(Object.values(circle(3))[1], 28.259999999999);
+    assert.strictEqual(Object.values(circle(3))[1], 28.259999999999998);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), circle3);
   });
 });
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,37 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    const test = productDetails('Alcool gel', 'Máscara');
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof (test), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(test.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (test[0]), 'object');
+    assert.strictEqual(typeof (test[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(test[0],test[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    const product1 = test[0].details.productId;
+    const product2 = test[1].details.productId;
+    let checkProduct1 = 0;
+    let checkProduct2 = 0;
+    if (product1[product1.length-1] === '3') {
+      if (product1[product1.length-2] === '2') {
+        if (product1[product1.length-3] === '1') {
+          checkProduct1 = 1;
+        }
+      }
+    }
+    assert.strictEqual(checkProduct1, 1);
+    if (product2[product2.length-1] === '3') {
+      if (product2[product2.length-2] === '2') {
+        if (product2[product2.length-3] === '1') {
+          checkProduct2 = 1;
+        }
+      }
+    }
+    assert.strictEqual(checkProduct2,1);
   });
 });
