ffe22ba52115a8010e88099f529897ee705a8eb0
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-11-26 20:28:00 -0300

    REQ-6: productDetails solved

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len*/
 /* eslint-disable no-unused-vars */
 
-const assert = require('assert');
-const productDetails = require('../src/productDetails');
+const assert = require("assert");
+const productDetails = require("../src/productDetails");
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -31,14 +31,27 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('#productDetails', () => {
-  it('tests the function has the correct behaviour', () => {
-    assert.fail();
+describe("#productDetails", () => {
+  it("tests the function has the correct behaviour", () => {
     // ESCREVA SEUS TESTES ABAIXO:
+    const testArray = productDetails("comb", "pen");
+    const productIdNumbers = (index) => {
+      const obj = testArray[index];
+      const id = obj.details.productId;
+      return id.slice(-3);
+    };
+    console.log(productIdNumbers(0));
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(testArray), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(testArray.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof testArray[0], "object");
+    assert.strictEqual(typeof testArray[1], "object");
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(testArray[0], testArray[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productIdNumbers(0), '123');
+    assert.strictEqual(productIdNumbers(1), '123');
   });
 });
