be11c6c5e32a12d9c61478a04f83550b5f0d9443
enicio <jessandro42@gmail.com>
2020-11-26 16:12:55 -0300

    Primeiro requisito -  com ajustes

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,17 +1,3 @@
-/*
-  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
-  Caso a função receba algum valor não númerico ou um array vazio,
-  o valor undefined deve ser retornado.
-  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
-
-  Parâmetros:
-    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
-  Comportamento:
-    - average([2, 2]) // Retorno: 2;
-    - average([1, 1]) // Retorno: 1;
-    - average([1, '2']) // Retorno: undefined;
-*/
-
 function quantityOfZero(array) {
   let quantityOfZero = 0;
   for (let index = 0; index < array.length; index += 1) {
@@ -24,9 +10,10 @@ function quantityOfZero(array) {
   }
 }
 
+
 const average = (array) => {
   let sum = 0;
-
+  
   if (array.length === 0 || array.length === 1) {
     return undefined
   }
@@ -36,7 +23,6 @@ const average = (array) => {
       return undefined
     }
   }
-
   for (let index = 0; index < array.length; index += 1) {
     sum += array[index]
   }
@@ -44,6 +30,7 @@ const average = (array) => {
   quantityOfZero(array)
 
   return Math.round(sum / array.length)
-};
 
-module.exports = average;
+
+};
+console.log(average([' ']))
