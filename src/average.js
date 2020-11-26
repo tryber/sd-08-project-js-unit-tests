4731f5889ccc777db96a398d0750090d0ded3fd5
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-26 17:07:36 -0300

    Função average passou no npm test

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let cont = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    } else {
+      cont += array[i];
+    }
+  }
+  return Math.round(cont / array.length);
+};
 
 module.exports = average;
