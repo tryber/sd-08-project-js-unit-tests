530bface6e3eb3a8372c06a030146c70cd8eb0df
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-11-26 15:34:30 -0300

    correção requisito 4 - circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,7 +27,7 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.strictEqual(typeof (circle(5), 'object')
+    assert.strictEqual(typeof (circle(5)), 'object')
     // Teste se o objeto retornado tem 3 entradas.
     assert.strictEqual(Object.keys(circle(7)).length, 3)
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
@@ -37,7 +37,9 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     assert.strictEqual(circle(3).area, 3.14 * 3 * 3)
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.strictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 })
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 })
+
+    
 
   });
 });
