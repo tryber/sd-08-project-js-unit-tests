069363bb17d472f67b177bb032a13bbb00cc0205
Luiz Reina <luizreeina@gmail.com>
2020-11-26 17:20:12 -0300

    requisitos 2 e 3 - codeclimate

--- src/vqv.js
+++ src/vqv.js
@@ -24,7 +24,7 @@ const vqv = (nome, idade) => {
   const result = `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
+#VQV!`;
 
   return result;
 };
