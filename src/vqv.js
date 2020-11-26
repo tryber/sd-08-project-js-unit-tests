eca361b9acf58ccc73d2838eab8968742f518d9a
Tandy <tandyfw@gmail.com>
2020-11-26 17:44:51 -0300

    Requisito 3 codeclimate

--- src/vqv.js
+++ src/vqv.js
@@ -18,7 +18,7 @@
 
 const vqv = (nome, idade) => {
   if (nome) {
-    return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`
+    return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
   }
   return undefined;
 };
