3464584d235b4307a765c4595a0fb2ef42309a1c
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-26 17:00:54 -0300

    ultimo do req1, chama no :checked:

--- src/average.js
+++ src/average.js
@@ -18,7 +18,7 @@ const average = (arr) => {
     return undefined;
   }
   for (let i = 0; i < arr.length; i += 1) {
-    if (isNaN(arr[i])) {
+    if (typeof arrNum[index] === 'string'){
       return undefined;
     }
     somatorioArray += arr[i];
