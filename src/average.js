371458dbb26d92295f1ba7a8ac87313efc2a088f
liisaambrosen <liisa.ambrosen@gmail.com>
2020-11-26 16:36:40 -0300

    adiciona requisito 1 e 2

--- src/average.js
+++ src/average.js
@@ -12,6 +12,26 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+
+
+const average = (array) => {
+  let sum = 0;
+  let result;
+  const arrayVazio = [];
+  if (array.length <= 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+    };
+  result = Math.round(sum / array.length);
+  return result;
+};
+
 
 module.exports = average;
+
+
