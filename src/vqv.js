4a008ea163dd51378cc11a2c73a02ea4d9bb64d7
Lucas Gama <lucasbg@protonmail.com>
2020-11-26 18:16:43 -0300

    Requisito 3: codeclimate

--- src/vqv.js
+++ src/vqv.js
@@ -16,12 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) =>
-  nome === undefined
-    ? undefined
-    : `Oi, meu nome é ${nome}!
+function vqv(nome, idade) {
+  const vqvString =
+    nome === undefined
+      ? undefined
+      : `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
+  return vqvString;
+}
 
 module.exports = vqv;
