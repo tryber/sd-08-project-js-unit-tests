11f0ac274d67f898cd1781b24bd4b9ee6ee63495
victor Farias <victorfarias.tv@gmail.com>
2020-11-26 15:32:17 -0300

    Adicionado exercicio average

--- src/average.js
+++ src/average.js
@@ -12,6 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
+  let media = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    } else {
+      soma += array[i];
+    }
+  }
+  media = soma / array.length;
+  mediaArredondada = Math.round(media);
+  return mediaArredondada;
+};
 
 module.exports = average;
