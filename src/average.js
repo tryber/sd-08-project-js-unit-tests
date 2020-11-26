a28bc9e7c0d86a831a3d2167b9554127734c81f9
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-26 17:58:30 -0300

    Refatorei o average pois estava dando complexity 6

--- src/average.js
+++ src/average.js
@@ -13,21 +13,17 @@
 */
 
 const average = (array) => {
-  if (array.length === 0) {
-    return undefined;
-  }
-  let cont = 0;
-  for (let i = 0; i < array.length; i += 1) {
-    if (typeof array[i] !== 'number') {
-      return undefined;
+  if (array.length !== 0) {
+    let cont = 0;
+    for (let i = 0; i < array.length; i += 1) {
+      if (typeof array[i] !== 'number') {
+        return undefined;
+      } else {
+        cont += array[i];
+      }
     }
-
-    if (typeof array[i] === 'number') {
-      cont += array[i];
-    }
+    return Math.round(cont / array.length);
   }
-
-  return Math.round(cont / array.length);
 };
 
 module.exports = average;
