1cceb379b3888ead29252e71d5d9f0ac01fbb660
Rafael <rafaelmaltez89@gmail.com>
2020-11-26 18:31:54 -0300

    Requisite 03 codeclimate corrections

--- src/vqv.js
+++ src/vqv.js
@@ -17,13 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof nome == 'undefined' && typeof idade == 'undefined') {
+  if (typeof nome === 'undefined' && typeof idade === 'undefined') {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!\n` +
   `Tenho ${idade} anos,\n` +
-  `trabalho na Trybe e mando muito em programação!\n` +
-  `#VQV!`
+  'trabalho na Trybe e mando muito em programação!\n' +
+  '#VQV!';
 };
 
 module.exports = vqv;
