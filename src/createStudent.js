419ead9bae4ed974d1e907127b1f9941b3c8b002
rafasysop <rafaelcostamoura@gmail.com>
2020-11-26 19:30:29 -0300

    Code climate 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,9 +16,12 @@
 */
 
 const createStudent = (name) => {
-  return {name, feedback: () => {
+  return {
+    name, 
+    feedback: () => { 
     return 'Eita pessoa boa!';
-  }};
+  }
+};
 };
 
 module.exports = createStudent;
