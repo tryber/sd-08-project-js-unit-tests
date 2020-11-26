eb90a9315b291dcb0d72ac227e6e67507fa68f82
Ediberto Oliveira <edibertooliveira@aol.com>
2020-11-26 15:14:10 -0300

    feat: added test numbers requirement 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,12 +16,17 @@ const numbers = require('../src/numbers');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
+const parament = [1, 2, 3, 4, 5];
+const unexpected = [1, 2, '3', 4, 5];
+const expectedResult = true;
+const unexpectedResult = false;
+const output = numbers(parament);
+
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
-    assert.fail();
-    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    // Escreva um teste em que a função recebe [' '] e retorna false
+    assert.strictEqual(numbers(parament), expectedResult); // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    assert.strictEqual(numbers(unexpected), unexpectedResult); // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    assert.strictEqual(numbers([1, 'a', 3]), unexpectedResult); // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    assert.strictEqual(numbers([' ']), unexpectedResult); // Escreva um teste em que a função recebe [' '] e retorna false
   });
 });
