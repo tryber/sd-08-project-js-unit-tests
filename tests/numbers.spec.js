eb37d76e76d0f20023fa10f155f61803dae38e20
jnpefo <jnpefo@gmail.com>
2020-11-26 18:29:03 -0300

    Req 2, tentativa 1

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,7 +18,11 @@ const numbers = require('../src/numbers');
 
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
-    assert.fail();
+    // assert.fail();
+    assert.ok([1, 2, 3, 4, 5], true);
+    assert.ok([1, 2, '3', 4, 5], false);
+    assert.ok([1, 'a', 3], false);
+    assert.ok([' '], false);
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
