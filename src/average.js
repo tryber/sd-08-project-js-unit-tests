ec9e915de8ee3873cd607e8cc0f024d02a61b5ae
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-11-26 16:01:01 -0300

    Submetendo requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  let sum = 0;
+  if (arr.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < arr.length; index += 1) {
+    if (typeof arr[index] !== 'number') {
+      return undefined;
+    }
+    sum += arr[index];
+  }
+  return Math.round(sum / arr.length);
+};
 
 module.exports = average;
