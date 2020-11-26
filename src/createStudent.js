ee61bf9224c71927232cc94cba2254ed62c781a0
maym4y <jennim.almeida@gmail.com>
2020-11-26 19:35:10 -0300

    createStudent terminado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+function createStudent(nom) {
+  return {
+    name: nom,
+    feedback: () => {return 'Eita pessoa boa!';}
+  };
+};
 
 module.exports = createStudent;
