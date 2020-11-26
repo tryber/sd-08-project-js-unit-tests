de811f4e9d590069aaabfb4e0665b53397d5bd6e
Daniel <danieldsrd@gmail.com>
2020-11-26 18:48:35 -0300

    Exercicio 1

--- src/average.js
+++ src/average.js
@@ -17,7 +17,7 @@ const average = (array) => {
   if (array.length === 0) {
     return undefined;
   }
-  for (let index = 0; index < array.length; index+=1) {
+  for (let index = 0; index < array.length; index +=1) {
     if (typeof array[index] === 'number') {
       sum += array[index];
     } else {
