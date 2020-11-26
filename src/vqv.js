84174b4aa86ebc04ce447a42c239cb5b77cd47b9
Nícolas Tozato <ntozato9@gmail.com>
2020-11-26 19:43:30 -0300

    tentativa requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,18 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  let result;
   if (nome === undefined && idade === undefined) {
     return undefined;
   }
-
-  if (typeof nome === 'string' && typeof idade === 'number') {
-    result = `Oi, meu nome é ${nome}!
+  return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
-  #VQV!`;
-  }
-  return result;
+  #VQV!`
 };
 
 module.exports = vqv;
