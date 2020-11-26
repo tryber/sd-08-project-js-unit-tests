951983a19f25877fdc9bae0c515d6dbab3183354
liisaambrosen <liisa.ambrosen@gmail.com>
2020-11-26 17:45:43 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,6 +17,9 @@
 */
 
 function vqv(nome, idade) {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
