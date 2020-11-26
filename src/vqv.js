8ade8c899fb702d48e3fe88b91510d3cabc5637a
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-26 17:22:39 -0300

    req 3, agora vai

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,7 @@
 
 const vqv = (nome, idade) => `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!`;
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
 
 module.exports = vqv;
