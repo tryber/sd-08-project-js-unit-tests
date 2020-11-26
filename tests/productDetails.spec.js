eac0c6efa5e13ee1260061ce6fb686ea64ef7926
Clênio Galvão <clenio.cgm@gmail.com>
2020-11-26 16:59:34 -0300

    resolve questao 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,11 +36,24 @@ describe('#productDetails', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
     const result = productDetails('Alcool gel', 'Máscara');
-    assert.strictEqual(typeof result, 'array');
-    
+    const testArray = Object.keys(result);
+    assert.deepStrictEqual(testArray[0], '0');
+
     // Teste que o array retornado pela função contém dois itens dentro.
+    const tamResult = result.length;
+    assert.deepStrictEqual(tamResult, 2);
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof result[0], 'object');
+    assert.deepStrictEqual(typeof result[1], 'object');
+
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(result[0], result[1]);
+
     // (Difícil) Teste que os dois productIds terminam com 123.
+    const test1 = result[0].details.productId.substring(result[0].details.productId.length - 3);
+    assert.deepStrictEqual(test1, '123');
+    const test2 = result[1].details.productId.substring(result[1].details.productId.length - 3);
+    assert.deepStrictEqual(test2, '123');
   });
 });
