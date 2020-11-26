c179dffbe6f1f6a4b10bead6b31f43da65002f98
Bruno Martins <bruno.martins217@gmail.com>
2020-11-26 16:33:13 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-
+const average = (array) => {
+  let total = 0;
+  let soma = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number' || array.length === 0) {
+      return undefined;
+    }
+    soma += array[i];
+    total = soma / array.length;
+  }
+  return Math.round(total);
+};
+console.log(average([]));
 module.exports = average;
