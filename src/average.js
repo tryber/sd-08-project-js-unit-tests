28f632a3fb0d05efa328b2ba77d95b52c52cdb45
Douglas Ferreira <dodofp.df@gmail.com>
2020-11-26 17:24:07 -0300

    Teste requisito 1

--- src/average.js
+++ src/average.js
@@ -16,7 +16,7 @@ const average = (array) => {
   let sum = 0;
   for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') {
-      return 'undefined';
+      return undefined;
     }
     sum += array[index];
   }
