64daeb8d481999ee8c11e539231f8bd23c59d0e7
Tandy <tandyfw@gmail.com>
2020-11-26 18:23:19 -0300

    Requisito 5 concluido

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => ({
+  name: nome,
+  feedback: feedbackfunction
+});
+
+const feedbackfunction = () => 'Eita pessoa boa!';
 
 module.exports = createStudent;
