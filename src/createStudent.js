fa052b20bfa15e31513d38fa1ed5d05248e383e7
lpj <lpj@DESKTOP-LPBJ548.SrJamal>
2020-11-26 19:09:50 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const estudante = {
+    name: name,
+    feedback: () => {
+      return 'Eita pessoa boa!';
+    },
+  };
+  return estudante;
+};
 
 module.exports = createStudent;
