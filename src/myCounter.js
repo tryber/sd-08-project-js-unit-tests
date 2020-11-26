b215f56365382bf90cfda29bc3f6f27d9462e118
Victor Leone de Oliveira <vileoleone@gmail.com>
2020-11-26 18:59:59 -0300

    Requisito_8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,10 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
+    for (counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
   return myArray;
 };
-console.log(myCounter())
 module.exports = myCounter;
