f2858c59a70e9b3372029827a54cf5943af8fd35
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-11-26 14:18:40 -0300

    adicionando requisito 3 CC

--- src/vqv.js
+++ src/vqv.js
@@ -20,6 +20,7 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
+  
   return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
