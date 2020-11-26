44af62b302771295aa90c06b564953a6da6ac6be
Douglas Ferreira <dodofp.df@gmail.com>
2020-11-26 17:20:21 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -14,13 +14,13 @@
 
 const average = (array) => {
   let sum = 0;
-  for (let index = 0; index < array.length; index +=1 ) {
+  for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') {
       return 'undefined';
     }
-    sum += index;
+    sum += array[index];
   }
-  return index
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
