5328d9b3ee2e95801b2b4cd46ec1209eae7be984
Felipe Segovia <felipe.vsegovia@gmail.com>
2020-11-26 17:12:07 -0300

    Requisito 1 alterado

--- src/average.js
+++ src/average.js
@@ -12,6 +12,16 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += (array[index]);
+    result = (sum / array.length);
+  }
+  return result;
+};
 
 module.exports = average;
