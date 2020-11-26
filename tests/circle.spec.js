d8133d02585bab335fce5c84dacba7698c542aed
maym4y <jennim.almeida@gmail.com>
2020-11-26 19:04:55 -0300

    Finalizado teste unitário de Circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -22,7 +22,7 @@ const circle = require('../src/circle');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
-const compare = {radius: 3, area: 28.26, circumference: 18.84};
+const compare = {radius: 3, area: 28.259999999999998, circumference: 18.84};
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
@@ -36,7 +36,7 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.deepStrictEqual(circle(2)['circumference'], 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.deepStrictEqual(circle(3)['area'], 28.26)
+    assert.deepStrictEqual(circle(3)['area'], 28.259999999999998);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3), compare);
   });
