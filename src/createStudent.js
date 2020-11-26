ac032523c492e0758d561cd6658b9780c2f45435
Mateus Coury <mateuscoury@gmail.com>
2020-11-26 18:29:08 -0300

    cc3

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,5 +14,8 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-const createStudent = (name) => ({ name, feedback: () => 'Eita pessoa boa!' });
+const createStudent = name => ({
+  name,
+  feedback: () => 'Eita pessoa boa!',
+});
 module.exports = createStudent;
