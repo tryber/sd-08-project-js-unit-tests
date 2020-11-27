6fea7c21efaefd26e894993ff097f87d2ff1f2f5
MuriloBatista <mub4si@gmail.com>
2020-11-27 15:13:26 -0300

    Trabalhando no 6º requisito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 const assert = require('assert');
+const { arch } = require('os');
 const productDetails = require('../src/productDetails');
 
 /*
@@ -33,12 +34,15 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // const resultado = productDetails('Alcool gel', 'Máscara');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
     // Teste que o array retornado pela função contém dois itens dentro.
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
+    // assert.ok(productDetails('Alcool gel', 'Máscara'), Array.isArray);
+    // assert.ok(productDetails('Alcool gel', 'Máscara'), Array.length === 2);
+    // assert.ok(typeof resultado, 'objec');
   });
 });
