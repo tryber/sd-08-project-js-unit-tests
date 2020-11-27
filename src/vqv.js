381ffd617da046852631794284d983a1e6b57c13
antonioarieiro <sfgfffs@gmail.com>
2020-11-27 10:22:44 -0300

    fix codeclimate vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,5 +16,5 @@
       #VQV!'
 */
 
-const vqv = (name, age) => `Oi, meu nome é ${name}! Tenho ${age} anos, trabalho na Trybe e mando muito em programação! #VQV!'`
+const vqv = (name, age) => `Oi, meu nome é ${name}! Tenho ${age} anos, trabalho na Trybe e mando muito em programação! #VQV!'`;
 module.exports = vqv;
