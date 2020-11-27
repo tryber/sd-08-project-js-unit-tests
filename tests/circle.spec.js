15ebc8c4620c967d3229111347496d784173183d
wagnerberna <cabelo29007@gmail.com>
2020-11-27 09:54:10 -0300

    req04

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,31 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    const circuloTeste = circle(2);
+    const circuloTesteArea3 = circle(3);
+    const quantEl = Object.values(circuloTeste).length;
+    const circuloTesteVazio = circle();
+
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circuloTeste, 'object', 'Erro Objeto');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.ok(quantEl === 3, 'objeto têm de ter 3 elementos');
+    
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(typeof circuloTesteVazio, 'undefined', 'teste sem parâmetros');
+    
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.ok(circuloTeste.circumference === 12.56, 'teste circunf. 2');
+    
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.ok(circuloTesteArea3.area === 28.259999999999998, 'teste area 3');
+
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    testA3C = circuloTesteArea3.circumference === 18.84;
+    testA3R = circuloTesteArea3.radius === 3;
+    testA3A = circuloTesteArea3.area === 28.259999999999998;
+    testA3All = testA3C && testA3R && testA3A;
+    assert.ok(testA3All === true, 'teste area 3');
   });
 });
