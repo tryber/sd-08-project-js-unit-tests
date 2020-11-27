ccb0b30451d3d6bb3d33f67abeb1656e7ea4e3b4
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2020-11-27 11:27:37 -0300

    Requisito 5 finalizado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const xablau = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return xablau;
+};
 
 module.exports = createStudent;
--- src/vqv.js
+++ src/vqv.js
@@ -17,10 +17,9 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === '' || idade === '') {
-    return undefined; 
-  } else {
-    return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  if (nome.length === '' || idade === '') {
+    return undefined;
   }
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 module.exports = vqv;
