ff6eac3ec88d32dddf4823ec30718823d84f7e4f
Nícolas Godek <nicolasgodek@gmail.com>
2020-11-27 02:00:46 -0300

    arrumando requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,8 +25,8 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.strictEqual(typeof circle(), 'object');
-    assert.strictEqual(Object.keys(circle()).length, 3);
+    assert.strictEqual(typeof circle(1), 'object');
+    assert.strictEqual(Object.keys(circle(1)).length, 3);
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56);
     assert.strictEqual(circle(3).area, 28.259999999999998);
