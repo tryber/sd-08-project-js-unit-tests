176f91081a448a65cd4617539636baff38c0f97d
josebejohann <josebernardojohann@gmail.com>
2020-11-27 10:08:58 -0300

    Trying to fix the circle.js code

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -35,13 +35,19 @@ describe('#circle', () => {
     assert.strictEqual(circle(), undefined);
 
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.deepStrictEqual(circle(2).circumference, 13);
+    assert.deepStrictEqual(
+      parseInt(parseFloat(circle(2).circumference).toPrecision(2), 10),
+      13
+    );
 
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.deepStrictEqual(circle(2).area, 13);
+    assert.deepStrictEqual(
+      parseInt(parseFloat(circle(2).area).toPrecision(2), 10),
+      13
+    );
 
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(circle(3), {
+    assert.deepStrictEqual(parseInt(parseFloat(circle(3)).toPrecision(2), 10), {
       radius: 3,
       area: 28,
       circumference: 19,
