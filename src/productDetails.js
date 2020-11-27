ad7edb9ed85220c896201afc0508b023ee2a7c87
Rafael <rafaelmaltez89@gmail.com>
2020-11-27 12:41:49 -0300

    productDetails codeclimate corrections

--- src/productDetails.js
+++ src/productDetails.js
@@ -40,4 +40,3 @@ const productDetails = (firstProduct, secondProduct) => [
 ];
 
 module.exports = productDetails;
-console.log(productDetails('Alcool gel', 'Máscara')[0].details.productId.endsWith('123'))
