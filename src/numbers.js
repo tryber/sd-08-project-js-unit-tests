696d02fb0551b222217a4126870187d193fc5240
antonioarieiro <sfgfffs@gmail.com>
2020-11-27 08:40:50 -0300

    numbers

--- src/numbers.js
+++ src/numbers.js
@@ -1,23 +1,19 @@
-/*
-  A função numbers recebe um array (tamanho variável),
-  retornando true se todos os parâmetros forem do tipo 'number' e false caso contrário.
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
 
+const assert = require('assert');
+const numbers = require('../src/numbers');
+
+/*
+  A função numbers recebe um array (tamanho variável) e retorna true se todos os parâmetros forem do tipo 'number' e false caso contrário.
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, 'a']; [].
   Comportamento:
     - numbers([2, 3, 4]); // Retorna: true
     - numbers([2, 'errado', 5]); // Retorna: false
+  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
-const assert = require('assert');
 
-const numbers = (myArray) => {
-  for (let i = 0; i < myArray.length; i += 1) {
-    if (typeof myArray[i] !== 'number') {
-      return false;
-    }
-  }
-  return true;
-};
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
@@ -30,4 +26,3 @@ describe('#numbers', () => {
     assert.strictEqual(numbers([' ']), false);
   });
 });
-module.exports = numbers;
