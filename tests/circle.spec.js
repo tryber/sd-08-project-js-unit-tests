f4a510aab4c8bacea4e14c22268bb55589c56d85
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-27 16:10:53 -0300

    circumference

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,10 +28,9 @@ describe('#circle', () => {
   
     // ESCREVA SEUS TESTES ABAIXO:    
     assert.strictEqual(typeof circle(3), 'object');// Teste se circle retorna um objeto.
-    assert.strictEqual(Object.keys(circle(3)).length, 3);
-    // Teste se o objeto retornado tem 3 entradas.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(Object.keys(circle(3)).length, 3);// Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(circle(), undefined);// Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(2).circumference, 12.56)// Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
