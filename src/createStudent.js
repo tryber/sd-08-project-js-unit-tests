bd4cb0007a6ab83eb40ab55d5131e6b20f6f0667
Maurice Alves <maurice.estevam@gmail.com>
2020-11-27 16:28:04 -0300

    Requisito - 5.

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (param) => {
+  const estudante = {
+    name: param,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
+};
 module.exports = createStudent;
