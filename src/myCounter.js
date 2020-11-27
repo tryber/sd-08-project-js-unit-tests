bb7a88f805bfebd748122bc561b2bc3b9a664f7a
enicio <jessandro42@gmail.com>
2020-11-27 00:04:29 -0300

    MyCounter - myArray alterado para o tipo const

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,7 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counterCont = 2; counterCont <= 3; counterCont += 1) {
