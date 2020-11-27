55fd10dffef227785ba1bf3a25be70edbccbf0f3
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2020-11-27 15:34:48 -0300

    Requisito 3 - Correçao apos a sugestao do codeclimate (2)

--- src/vqv.js
+++ src/vqv.js
@@ -18,9 +18,8 @@
 
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
-    return undefined; 
-  } else {
-    return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+    return undefined;
   }
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 module.exports = vqv;
