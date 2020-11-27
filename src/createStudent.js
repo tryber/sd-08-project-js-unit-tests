127ebdbe869d4c3bf375ba0de0308c25b2bd83c6
Rafael <rafaeljavaro750@gmail.com>
2020-11-27 15:59:43 -0300

    Req createStudent.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const student = {};
+  student.name = name;
+  student.feedback = function() {
+    return 'Eita pessoa boa!'
+  }
+  return student;
+};
 
 module.exports = createStudent;
