db77a618e4e16adb0a346489d5f188a455625a53
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-27 12:07:32 -0300

    add rq3 no done yet all tests

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const feedback = () => 'Eita pessoa boa!'
+const createStudent = (name) => {
+  const student = {
+    name: name,
+    feedback: feedback(),
+  }
+  return student
+};
+
 
 module.exports = createStudent;
