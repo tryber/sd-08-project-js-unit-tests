7adeea18172db8014a8805d98de9437f30f8b78a
rafasysop <rafaelcostamoura@gmail.com>
2020-11-26 22:12:16 -0300

    Requisito 9, teste 1

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -51,8 +51,10 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
+    const objMenu = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} };
+    const meuRestaurante = createMenu({ food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} })
+    assert.deepStrictEqual(createMenu(objMenu), {"fetchMenu": {"drink": {"agua": 3.9, "cerveja": 6.9}, "food": {"coxinha": 3.9, "sopa": 9.9}}});
     // ```
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
     // ```
