3cad57b1e653a0845478d1161ee33be9a5516896
Fernanda Marques <nandateixmarques@gmail.com>
2020-11-27 09:29:17 -0300

    arrumando o CodeClimate. novamente 2.

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,7 +19,7 @@ const myCounter = () => {
   var myArray = [];
   for (var counter1 = 0; counter1 <= 3; counter1 += 1) {
     myArray.push(counter1);
-    for (var counter2 = 2; counter2 <= 3; counter2+= 1) {
+    for (var counter2 = 2; counter2 <= 3; counter2 += 1) {
       myArray.push(counter2);
     }
   }
