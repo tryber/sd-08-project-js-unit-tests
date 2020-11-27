0eec4d6e58bdf84922e3bb77d0bb080eb1b21dd9
Thiago Ferreira <thiagocristhian@gmail.com>
2020-11-26 21:05:53 -0300

    Requisito 3 - adicionando função

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,6 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 
 module.exports = vqv;
