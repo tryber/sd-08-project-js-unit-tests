7a8ebdf8228dff37744e575975dfad4e0427b2cb
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-27 17:26:13 -0300

    Requisite vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  let stringVQV = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
+
+  return stringVQV;
+};
 
 module.exports = vqv;
