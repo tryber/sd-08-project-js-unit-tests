ab86f3b53f26bcfb415b9584f8d316d13f66efb7
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2020-11-27 18:33:21 -0300

    adiciona funcao createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const feedback = () => 'Eita pessoa boa!';
+  const obj = {
+    name,
+    feedback,
+  };
+  return obj;
+};
 
 module.exports = createStudent;
