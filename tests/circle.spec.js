188a44fe25eb5e96e75cf1aaa6efec2ad6ef3904
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-27 17:54:44 -0300

    deepStrictEqual

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,33 +25,15 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-  
-    // ESCREVA SEUS TESTES ABAIXO:    
-    assert.strictEqual(typeof circle(3), 'object');// Teste se circle retorna um objeto.
-    assert.strictEqual(Object.keys(circle(3)).length, 3);// Teste se o objeto retornado tem 3 entradas.
-    assert.strictEqual(circle(), undefined);// Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    assert.strictEqual(circle(2).circumference, 12.56)// Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.strictEqual(typeof circle(3), 'object'); // Teste se circle retorna um objeto.
+    assert.strictEqual(Object.keys(circle(3)).length, 3); // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(circle(), undefined); // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(2).circumference, 12.56); // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(circle(3).area, 28.259999999999998); // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), {
+      area: 28.259999999999998,
+      circumference: 18.84,
+      radius: 3,
+    }); // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
-
-// it('returns the object as specified', () => {
-//   const estudante = createStudent('Leandrão, o Lobo Solitário');
-//   assert.strictEqual(typeof estudante, 'object');
-//   assert.strictEqual(typeof estudante.feedback, 'function');
-//   assert.strictEqual(estudante.name, 'Leandrão, o Lobo Solitário');
-//   assert.strictEqual(estudante.feedback(), 'Eita pessoa boa!');
-
-//   const estudante2 = createStudent('Nobre');
-//   assert.strictEqual(typeof estudante2, 'object');
-//   assert.strictEqual(typeof estudante2.feedback, 'function');
-//   assert.strictEqual(estudante2.name, 'Nobre');
-//   assert.strictEqual(estudante2.feedback(), 'Eita pessoa boa!');
-
-//   const estudante3 = createStudent('Inácio');
-//   assert.strictEqual(typeof estudante3, 'object');
-//   assert.strictEqual(typeof estudante3.feedback, 'function');
-//   assert.strictEqual(estudante3.name, 'Inácio');
-//   assert.strictEqual(estudante3.feedback(), 'Eita pessoa boa!');
-// });
