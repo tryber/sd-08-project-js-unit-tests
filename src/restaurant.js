33384222a176a96b3f60ee36916e33c3a1f112c3
Luiz Reina <luizreeina@gmail.com>
2020-11-26 22:03:46 -0300

    requisito final - codeclimate v2

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,9 +71,9 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const totalPrice = (orders, foodPrice, drinkPrice) => {
+function totalPrice(orders, foodPrice, drinkPrice) {
   let totalToPay = 0;
-  for (let index = 0; index < orders.length; index += 1) {    
+  for (let index = 0; index < orders.length; index += 1) {
     if (foodPrice[orders[index]] !== undefined) {
       totalToPay += foodPrice[orders[index]];
     }
@@ -101,7 +101,7 @@ const createMenu = (param) => {
   return menu;
 };
 
-const meuRestaurante = createMenu( { food: {coxinha: 3.9, sopa: 9.9}, drink: {agua: 3.9, cerveja: 6.9} });
+const meuRestaurante = createMenu({ food: { coxinha: 3.9, sopa: 9.9 }, drink: { agua: 3.9, cerveja: 6.9 } });
 
 meuRestaurante.order('coxinha');
 meuRestaurante.order('coxinha');
