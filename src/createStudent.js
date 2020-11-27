0a1d85afb5b5c7d4c7b3f81a9666e890cb241f35
momentts <marcosd3wow@gmail.com>
2020-11-27 16:02:17 -0300

    testando o quesito estudante

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (estudante) => {
+  const createObject = {
+    name: estudante,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return createObject;
+};
 
 module.exports = createStudent;
