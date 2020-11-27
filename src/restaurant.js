3804f437c7d0a160b19dfedd661f9966e8a9a4cb
Felipe Segovia <felipe.vsegovia@gmail.com>
2020-11-27 16:49:23 -0300

    restaurant codeclimate

--- src/restaurant.js
+++ src/restaurant.js
@@ -70,11 +70,8 @@
 
 // PASSO 4: Adicione ao objeto `restaurant`, que foi retornado pela função `createMenu()` uma chave `pay` com uma função que itera por todos os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará iterar tanto pelo objeto da chave `food` quanto pelo objeto da chave `drink`.
 
-<<<<<<< HEAD
-const createMenu = () => {}
-=======
-const createMenu = () => { };
->>>>>>> a665641a2d05cd8e245d673e7c0dd1ef8cddb40a
+const createMenu = () => {};
+
 
 console.log(fetchMenu);
 module.exports = createMenu;
