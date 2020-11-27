5345812ebb16dec55ab718f26a1b852242a98ea5
Hugo Andrade Braga <hugobrr@gmail.com>
2020-11-26 21:03:46 -0300

    8 requisito passando no local e cc passando no local - 77%

--- src/myCounter.js
+++ src/myCounter.js
@@ -12,15 +12,16 @@
   - Nenhum.
 
   Comportamento:
-  myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
+  myCounter() // Retorna:
+  [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
 const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var counter1 = 2; counter1 <= 3; counter1 += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
