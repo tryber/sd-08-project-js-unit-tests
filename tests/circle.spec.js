fe09ddbf49a2edc9b4dfd3266953d002a08125c5
Nícolas Tozato <ntozato9@gmail.com>
2020-11-26 21:40:29 -0300

    corrige circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -37,10 +37,6 @@ describe("#circle", () => {
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 2 * 3.14 * 2);
     assert.strictEqual(circle(3).area, 3.14 * 3 * 3);
-    assert.deepStrictEqual(circle(3), {
-      radius: 3,
-      area: 3.14 * 3 * 3,
-      circumference: 2 * 3,14 * 3
-    });
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 3.14 * 3 * 3, circumference: 2 * 3.14 * 3 });
   });
 });
