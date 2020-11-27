3474026c908d16292a81d25dc72c4308c472515e
Rafael <rafaeljavaro750@gmail.com>
2020-11-27 18:07:22 -0300

    Desfaz bagunça

--- src/numbers.js
+++ src/numbers.js
@@ -19,10 +19,3 @@ const numbers = (myArray) => {
 };
 
 module.exports = numbers;
-
-const assert = require('assert');
-
-assert.strictEqual(numbers([1, 2]), true);
-assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
-assert.strictEqual(numbers([1, 2, 'a']), false);
-assert.strictEqual(numbers([]), false);
