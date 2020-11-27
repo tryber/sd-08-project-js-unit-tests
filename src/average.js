bc3e802a3f3a09c23d093086b874a3416ee4ec8b
momentts <marcosd3wow@gmail.com>
2020-11-27 15:43:36 -0300

    testando o average

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let num = 0;
+  let avg = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    num += array[index];
+  }
+  avg = num / array.length;
+  return Math.round(avg);
+};
 
 module.exports = average;
