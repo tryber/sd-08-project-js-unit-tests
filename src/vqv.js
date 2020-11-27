c9fc9cdd2c16fcf753125c36f1b823688565deb3
Fernanda Marques <nandateixmarques@gmail.com>
2020-11-26 23:40:07 -0300

    adicionando requisitos 3 e 5

--- src/vqv.js
+++ src/vqv.js
@@ -17,11 +17,11 @@
 */
 
 const vqv = (nome, idade) => {
-  if(nome && idade) {
+  if (nome && idade) {
     const mensagem = `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
+#VQV!`;
     return mensagem;
   }
   return undefined;
