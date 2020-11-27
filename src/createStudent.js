06b2080e7433826c1457f4c6031d5b552fbf1551
wagnerberna <cabelo29007@gmail.com>
2020-11-27 10:12:23 -0300

    req05

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (nome) => {
+   return {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  }
+};
+const estudante = createStudent('wagner');
+//console.log(estudante);
+//console.log(estudante.feedback);
 module.exports = createStudent;
