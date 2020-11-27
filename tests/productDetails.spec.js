cf064af6b0f5768ba1b9dafe465ba03d2a3c084d
Nícolas Tozato <ntozato9@gmail.com>
2020-11-27 18:53:04 -0300

    productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -42,7 +42,7 @@ describe('#productDetails', () => {
     assert.strictEqual(typeof productDetails('firstProduct', 'secondProduct')[0], 'object');
     assert.strictEqual(typeof productDetails('firstProduct', 'secondProduct')[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
-    assert.notDeepStrictEqual(productDetails('firstProduct', 'secondProduct')[0], productDetails(firstProduct, secondProduct[1]));
+    assert.notDeepStrictEqual(productDetails('firstProduct', 'secondProduct')[0], productDetails('firstProduct', 'secondProduct')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
     assert.strictEqual(productDetails('firstProduct', 'secondProduct')[0].details.productId.slice(-3), '123');
     assert.strictEqual(productDetails('firstProduct', 'secondProduct')[1].details.productId.slice(-3), '123');
