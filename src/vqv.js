dce3dd8f9a14f7b92eb6cabebc93de2e006e87f7
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2020-11-26 23:47:19 -0300

    atualiza requisito 3 para o ultimo teste

--- src/vqv.js
+++ src/vqv.js
@@ -16,9 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => (`Oi, meu nome é ${nome}!
+const vqv = (nome, idade) => {
+  if (nome == undefined || idade == undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV`);
-console.log(vqv('Hamaji', 29));
+#VQV!`;
+}
+
 module.exports = vqv;
