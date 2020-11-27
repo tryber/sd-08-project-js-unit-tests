8f4af05746e2ad155861a0ff9c740aabe327f303
Maurice Alves <maurice.estevam@gmail.com>
2020-11-27 11:02:51 -0300

    Requisito - 3.Correção Codeclimate

--- src/vqv.js
+++ src/vqv.js
@@ -20,8 +20,7 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return;
   }
-  return `
-  Oi, meu nome é ${nome}!
+  return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
