2110e79a599df29e84ee9f0a645da54121418ddb
Rafael <rafaelmaltez89@gmail.com>
2020-11-27 12:36:55 -0300

    Requisite 06: productDetails test cases

--- src/productDetails.js
+++ src/productDetails.js
@@ -40,3 +40,4 @@ const productDetails = (firstProduct, secondProduct) => [
 ];
 
 module.exports = productDetails;
+console.log(productDetails('Alcool gel', 'Máscara')[0].details.productId.endsWith('123'))
