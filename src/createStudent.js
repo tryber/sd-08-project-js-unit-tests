c85d34bc3b850db82d436fe44492716e91d1bb9c
JRGCast <joao.ralph1@gmail.com>
2020-11-27 12:49:22 -0300

    realizado createStudent, sem entender a razao de feedback precisar ser uma function

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const addFeedback = {
+    name: name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return addFeedback;
+};
 
 module.exports = createStudent;
