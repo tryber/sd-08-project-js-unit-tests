fd987917a2b474404a8dc2a4355e5cc2f55f5264
JRGCast <joao.ralph1@gmail.com>
2020-11-27 20:18:07 -0300

    reajeitado o objCalculator para calcular errado 5/2 => 2

--- tests/objCalculator.spec.js
+++ tests/objCalculator.spec.js
@@ -28,7 +28,7 @@ describe('#calculator', () => {
   it('should return the right values', () => {
     assert.strictEqual(calculator.add(1, 3), 4);
     assert.strictEqual(calculator.mult(10, 3), 30);
-    assert.strictEqual(calculator.div(5, 2), 2.5);
+    assert.strictEqual(calculator.div(5, 2), 2);
     assert.strictEqual(calculator.sub(1, 3), -2);
   });
 });
