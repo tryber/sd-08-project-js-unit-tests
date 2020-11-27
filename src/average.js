1da160206ed861adacbd8c146ae1102110e6f52a
Uilton Rocha <itoclick@gmail.com>
2020-11-27 18:04:08 -0300

    AVERAGE projeto. VAMOS COM TUDO :rocket:

--- src/average.js
+++ src/average.js
@@ -17,13 +17,11 @@ const average = (param1) => {
   let tp;
 
   for (let i = 0; i < param1.length; i += 1) {
-    if (typeof (param1[i]) !== 'number'){return undefined;} 
+    if (typeof (param1[i]) !== 'number') {return undefined;} 
     tp = param1[i];
-    somartudo += tp; 
-    }
-    return Math.round(somartudo / param1.length);
+    somartudo += tp;
+  }
+  return Math.round(somartudo / param1.length);
 };
 
 module.exports = average;
-
-
