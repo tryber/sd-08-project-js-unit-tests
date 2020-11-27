d1ff3c1cb0c9a9ee009572d5ddc1b44da1725b00
Vitor Nunes <vitornunes200@gmail.com>
2020-11-27 18:11:09 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let num = 0;
+  const media;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number' || array[index] === '') {
+      return undefined;
+    }
+    num += array[index];
+  }
+  media = num / array.length;
+  return Math.round(media);
+};
 
 module.exports = average;
