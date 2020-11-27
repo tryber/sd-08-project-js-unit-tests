19c6fdf75569bdc40c74074878e721b47f52fac7
Pedro Piquini <piquini.pedro@gmail.com>
2020-11-26 23:03:59 -0300

    Feito oitava requisito e correçoes de codeclimate

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -21,9 +21,9 @@
 
 const calculator = {
   add: (num1, num2) => Math.floor(num1 + num2),
-  mult: (num1,num2) => Math.floor(num1 * num2),
-  div: (num1,num2) => Math.floor(num1 / num2),
-  sub: (num1,num2) => Math.floor(num1 - num2),
+  mult: (num1, num2) => Math.floor(num1 * num2),
+  div: (num1, num2) => Math.floor(num1 / num2),
+  sub: (num1, num2) => Math.floor(num1 - num2),
 };
 
 module.exports = calculator;
