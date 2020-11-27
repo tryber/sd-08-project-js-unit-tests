244d0e896557103ee378c86e5a0a2eeaf66a52b7
Nícolas Godek <nicolasgodek@gmail.com>
2020-11-27 02:44:47 -0300

    requisito 5 code climate

--- src/createStudent.js
+++ src/createStudent.js
@@ -19,8 +19,8 @@ const createStudent = (nome) => {
   const estudante = {
     name: nome,
     feedback: () => 'Eita pessoa boa!',
-}
-  return estudante
+  };
+  return estudante;
 };
 
 module.exports = createStudent;
