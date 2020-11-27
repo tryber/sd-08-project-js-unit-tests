4531a6bd170e19593cb1188b9be5689cadd9a94e
Rafael <rafaelmaltez89@gmail.com>
2020-11-27 12:12:14 -0300

    Circle test correction

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,12 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    // ESCREVA SEUS TESTES ABAIXO:
     assert.ok(typeof circle(1) === 'object');
     assert.ok(Object.keys(circle(2)).length === 3);
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56);
     assert.strictEqual(parseFloat(circle(3).area.toPrecision(4)), 28.26);
-    assert.strictEqual(circle(3), {radius: 3, area: 28.26, circumference: 18.84});
+    assert.deepStrictEqual(circle(3), {radius: 3, area: 28.259999999999998, circumference: 18.84});
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
