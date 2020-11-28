d7d97fb4c001758aaea75f5a7a6ca35f11b083a7
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-28 09:37:08 -0300

    Restaurando meus arquivos

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,8 +23,8 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('#numbers', () => {
-  it('should return an array and return if it has only numbers or not', () => {
+describe('#circle', () => {
+  it('given a radius, should return an object with circles info', () => {
     assert.deepStrictEqual(typeof circle(1), 'object');
     assert.strictEqual((Object.entries(circle(3)).length), 3);
     assert.strictEqual(circle(), undefined);
