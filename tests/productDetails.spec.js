5bff81913815d80f1cd6f2d25fbe6786fd5a32ce
Fernanda Marques <nandateixmarques@gmail.com>
2020-11-28 14:30:27 -0300

    adicionando requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,15 +30,21 @@ const productDetails = require('../src/productDetails');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
+const produtos = productDetails('Alcool gel', 'Máscara')
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(produtos), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(produtos.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof(produtos[0] && produtos[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual((produtos[0], produtos[1]), true)
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(produtos[0].details.productId.endsWith('123') === produtos[1].details.productId.endsWith('123'), true);
   });
 });
