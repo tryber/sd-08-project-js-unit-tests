753da6a3df6102afe8c244fc77c2bae22b1eead8
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-28 20:52:55 -0300

    Requesito 4, tentativa 1

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,16 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
+    assert.strictEqual(typeof circle(1), 'object');
+    assert.strictEqual(Object.keys(circle(1)).length, 3);
+    assert.strictEqual(circle(), undefined);
+    assert.strictEqual(Object.values(circle(2))[2], 12.56);
+    assert.strictEqual(Object.values(circle(3))[1], 28.259999999999998);
+    assert.strictEqual(circle(3), {
+      radius: 3,
+      area: 28.259999999999998,
+      circumference: 18.84
+    });
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
