77c045eef9bdbae789e517dadfc1cc73198d5410
Uilton Rocha <itoclick@gmail.com>
2020-11-28 03:01:53 -0300

    final averag

--- src/average.js
+++ src/average.js
@@ -17,12 +17,12 @@ const average = (param1) => {
   let tp;
   let a;
   for (let i = 0; i < param1.length; i += 1) {
-    if (typeof (param1[i]) !== 'number'){ return undefined; }
+    if (typeof (param1[i]) !== 'number') { return undefined; }
     tp = param1[i];
     somartudo += tp;
   }
   a = Math.round(somartudo / param1.length);
-  if (a === 'NaN') { a = 'undefined' };
+  if (a === 'NaN') { a = 'undefined'; }
   return a;
 };
 module.exports = average;
