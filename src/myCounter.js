4007338469e4aadde5951c4d78c2b6652a6695c0
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-28 11:54:30 -0300

    Codeclimate

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counterIn = 2; counterIn <= 3; counterIn += 1) {
+      myArray.push(counterIn);
     }
   }
   return myArray;
