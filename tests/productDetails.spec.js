530ee95715a5b89afcf79d6ebb7dc01cae494db3
Daniel Leon <daniel.radialista@gmail.com>
2020-11-28 14:28:37 -0300

    desafio 6, #VQV!

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len*/
 /* eslint-disable no-unused-vars */
 
+const { deepStrictEqual } = require('assert');
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
 
@@ -33,12 +34,18 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert(Array.isArray(productDetails('Alcool gel', 'Máscara'), 'array'));
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert(productDetails('Alcool gel', 'Máscara').length === 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert(typeof(productDetails('Alcool gel', 'Máscara')[0], [1]), 'Object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('Alcool gel', 'Máscara')[0],[1], 'Object');
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert(productDetails('Alcool gel', 'Máscara')[0].details.productId.slice(-3) === '123');
+    assert(productDetails('Alcool gel', 'Máscara')[1].details.productId.slice(-3) === '123');
   });
 });
