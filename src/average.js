39565f9fae3cd4edf46e3d7886fc43bc8a60c80d
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-27 21:07:16 -0300

    Primeiro commit

--- src/average.js
+++ src/average.js
@@ -12,6 +12,26 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const testingIfArrayIsNumber = (array) => {
+  if (array.length === 0) {
+    return false;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return false;
+    }
+  }
+}
+
+const average = (array) => {
+  if (testingIfArrayIsNumber === false) {
+    return undefined;
+  }
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    sum += array[index];
+  }
+  return Math.round(sum / array.length);
+};
 
 module.exports = average;
