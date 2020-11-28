6ddea3eca269c8b680d1754b2e4c67ffe90f167e
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-11-28 16:14:39 -0300

    envia req 3 novamente

--- src/vqv.js
+++ src/vqv.js
@@ -19,9 +19,9 @@
 const vqv = (nome, idade) => {
   if (nome && idade) {
     const message = `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
     return message;
   }
   return undefined;
