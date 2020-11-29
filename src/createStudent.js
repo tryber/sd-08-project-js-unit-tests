6ee81a3c18699b38662b7e169be2e326200391e7
Patrícia <silva.patricia.v@gmail.com>
2020-11-28 22:16:00 -0300

    atualiza arquivo createSudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,7 +15,7 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {
+const createStudent = (nome) => {
   const estudante = {
     name: nome,
     feedback: () => 'Eita pessoa boa!',
