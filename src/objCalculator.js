ccc2113afd73e5581dddc8658748b5342b0d6fe7
Pedro Amaral <amaralph1995@gmail.com>
2020-11-29 03:22:35 -0300

    Corrige codeclimate

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -23,7 +23,7 @@ const calculator = {
   add: (n1, n2) => n1 + n2,
   mult: (n1, n2) => n1 * n2,
   div: (n1, n2) => Math.floor(n1 / n2),
-  sub: (n1, n2) => n1 - n2
+  sub: (n1, n2) => n1 - n2,
 };
 
 module.exports = calculator;
