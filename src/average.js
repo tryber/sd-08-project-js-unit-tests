d1201a0e27e9908dcb5fe4c0f9ba153ce4038f1f
Luis Felipe Avila <lfelipeavila@outlook.com>
2020-11-29 16:09:37 -0300

    Req 1 e 2

--- src/average.js
+++ src/average.js
@@ -12,6 +12,15 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) return undefined;
+  let result = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] === 'string') return undefined;
+    result += array[i];
+  }
+  return Math.round(result = result / array.length);
+};
+console.log(average([]));
 
 module.exports = average;
