082929b041488d426835d0aab08590f3e1b75ff7
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-28 21:13:59 -0300

    Requesito 4, tentativa 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -37,7 +37,7 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     assert.strictEqual(Object.values(circle(3))[1], 28.259999999999998);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.strictEqual(circle(3), {
+    assert.deepStrictEqual(circle(3), {
       radius: 3,
       area: 28.259999999999998,
       circumference: 18.84,
