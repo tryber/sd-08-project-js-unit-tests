caa9613001b699202d3d3a305eed8a8e79bca849
Patrícia <silva.patricia.v@gmail.com>
2020-11-28 21:39:11 -0300

    code climate

--- src/average.js
+++ src/average.js
@@ -15,8 +15,8 @@
 const average = (array) => {
   if (array.length === 0) return undefined;
   let sum = 0;
-  for (let index = 0; index < array.length; index +=1) {
-    if (typeof array[index] ==='number') {
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] === 'number') {
         sum += array[index];
       } else {
         return undefined;
