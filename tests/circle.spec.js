c634e33752f16de5c4c34357ae81d1a973cc967b
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-11-29 17:21:18 -0300

    corrige valor esperado da area do circulo com raio 3 no ultimo teste

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -29,7 +29,7 @@ describe('#circle', () => {
     assert.strictEqual(Object.keys(circle(1)).length, 3);
     assert.strictEqual(circle(), undefined);   
     assert.strictEqual(circle(2).circumference, 12.56);
-    assert.strictEqual(circle(3).area, 28.26);
+    assert.strictEqual(circle(3).area, 28.259999999999998);
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
