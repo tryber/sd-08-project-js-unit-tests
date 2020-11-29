bd204e951cbc9c911951f87f75fbde80b7297da9
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-28 21:36:18 -0300

    Requesito 5, tentativa 2

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,14 +14,14 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-const feedback = () => {return 'Eita pessoa boa!'}
+const feedback = () => { return 'Eita pessoa boa!' };
 
 const createStudent = (name) => {
-  if (!name) {return undefined}
+  if (!name) { return undefined };
   return {
-  name,
-  feedback: feedback(),
-  }
+    name,
+    feedback: feedback(),
+  };
 };
 
 module.exports = createStudent;
