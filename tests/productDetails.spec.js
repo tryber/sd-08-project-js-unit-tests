050e78492a3ed78276f6422ecba2b57d6e3a349e
Patrícia <silva.patricia.v@gmail.com>
2020-11-28 21:12:38 -0300

    requisito 7

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len*/
 /* eslint-disable no-unused-vars */
 
+const { AssertionError } = require('assert');
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
 
@@ -33,12 +34,16 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails()[0] || typeof productDetails()[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails()[0] && productDetails()[1],true);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails()[0].details.productId.endsWith('123') && productDetails()[1].details.productId.endsWith('123'), true);
   });
 });
