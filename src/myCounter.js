0f16e8480f3f8980daf9c7d5e6276fef59123ce8
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-30 12:06:33 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,14 +14,13 @@
   Comportamento:
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
-const assert = require("assert");
 
 const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter <= 3; counter += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
