6d8a25108ef695a306ded0b5d1867214adf551f4
Uilton Rocha <itoclick@gmail.com>
2020-11-30 03:34:14 -0300

    teste vqv

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,11 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!
-  Tenho ${idade.toString()} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  return 'Oi, meu nome é ' + nome + '!\n' +
+  'Tenho ' + idade + ' anos,\n' +
+  'trabalho na Trybe e mando muito em programação!\n' +
+  '#VQV!'
+  
 };
 
 module.exports = vqv;
