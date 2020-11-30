e11c88709ac524378ab66af6c3e79505e693bbf9
amandaos <amanda.775.souza@gmail.com>
2020-11-30 19:06:28 -0300

    codeclimate requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,13 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof(nome) === 'undefined' && typeof(idade) === 'undefined') {
+  if (typeof (nome) === 'undefined' && typeof (idade) === 'undefined') {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
-  #VQV!`
+  #VQV!`;
 };
 
 module.exports = vqv;
