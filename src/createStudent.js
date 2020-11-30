7229d58f4c03fc7debb47723497620dc50b603bc
Pedro Alves <ped.alm2709@gmail.com>
2020-11-30 11:14:02 -0300

    funcao createStudant

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (estudante) => {
+  const studantObject = {
+    name = estudante,
+    feedback: () => 'Eita pessoa boa!'
+  };
+  return studantObject; 
+};
 
 module.exports = createStudent;
