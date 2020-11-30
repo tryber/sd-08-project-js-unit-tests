ff7c38723abae1d4762fa36646bc8f164c06a28b
Uibirá <usbarreto@gmail.com>
2020-11-30 19:10:42 -0300

    Reescreve a frase

--- src/vqv.js
+++ src/vqv.js
@@ -21,9 +21,9 @@ const vqv = (nome, idade) => {
     return undefined;
   }
   const message = `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
   return message;
 };
 
