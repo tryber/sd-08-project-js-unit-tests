536bdc22988db7d23b3ba8ca3f9030515b90c163
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-30 13:49:47 -0300

    Requisite circle

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,8 +33,10 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    let productArray = [];
+    productArray = (parseFloat(productDetails('a1', 'a2')));
+    assert.strictEqual(typeof productArray, 'array');
     // Teste que o retorno da função é um array.
     // Teste que o array retornado pela função contém dois itens dentro.
     // Teste que os dois itens dentro do array retornado pela função são objetos.
