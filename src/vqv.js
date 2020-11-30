a8dccf382fb8f38cb7409cc8d9e5b4771da4ec53
Pedro Alves <ped.alm2709@gmail.com>
2020-11-30 11:51:10 -0300

    vqv problem

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,11 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  const answer = `Oi, meu nome é ${nome}!
+  'Tenho ${idade} anos,
+  'trabalho na Trybe e mando muito em programação!
+  '#VQV!`;
+  return answer;
 };
 
 module.exports = vqv;
