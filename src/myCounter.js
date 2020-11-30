dcb6c3d988f25e0698e8caa5cb308550c814ce54
Joao <joaocarlitoneto@gmail.com>
2020-11-30 10:15:12 -0300

    approved requirement 08 without issues

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let otherCounter = 2; otherCounter <= 3; otherCounter += 1) {
+      myArray.push(otherCounter);
     }
   }
   return myArray;
