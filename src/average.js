110729df1fdccf2bb37e24fefc228746af61ab72
Erick Santos <erickgaves@gmail.com>
2020-11-30 12:38:44 -0300

    corrigindo codeclimate

--- src/average.js
+++ src/average.js
@@ -16,7 +16,7 @@ const myArray = [1, 2, 3, 4];
 const average = (array) => {
   if (array.length === 0) return undefined;
   let sum = 0;
-  for (let item in array) {
+  for (item in array) {
     if (typeof array[item] !== 'number') {
       return undefined;
     }
