9f2fb7a9874865d8eb39040b501a462677049e92
amandaos <amanda.775.souza@gmail.com>
2020-11-30 20:19:42 -0300

    altera requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,13 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof (nome) === 'string' && typeof (idade) === 'number') {
-    return `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
+  if (typeof (nome) !== 'string' && typeof (idade) !== 'number') {
+    return undefined;
   }
-  return undefined;
+  return `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,\n 
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
 };
 
 module.exports = vqv;
