817fdba18ffb27af50f655f2e7ad16bd1686910d
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-30 23:58:31 -0300

    Requisite productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,13 +34,20 @@ const productDetails = require('../src/productDetails');
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // ESCREVA SEUS TESTES ABAIXO:
-    let productArray = [];
-    productArray = (parseFloat(productDetails('a1', 'a2')));
-    assert.strictEqual(typeof productArray, 'array');
+    const productArray = productDetails();
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(Array.isArray(productArray), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(productArray.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productArray[0], 'object');
+    assert.deepStrictEqual(typeof productArray[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.deepStrictEqual(productArray[0] !== productArray[1], true);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    const productArraySlice0 = productArray[0].details.productId.slice(-3);
+    const productArraySlice1 = productArray[1].details.productId.slice(-3);
+    assert.deepStrictEqual(productArraySlice0, '123');
+    assert.deepStrictEqual(productArraySlice1, '123');
   });
 });
