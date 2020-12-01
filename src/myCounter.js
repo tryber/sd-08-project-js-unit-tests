85643b8dca1d5325ebe4ed91a32d40b6e70de091
WagnerMor <wagnermor@outlook.com>
2020-12-01 01:44:58 -0300

    requisito 08 codeclimate

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let iCounter = 2; iCounter <= 3; iCounter += 1) {
+      myArray.push(iCounter);
     }
   }
   return myArray;
