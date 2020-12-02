0479cf4589e0e6a44881854c6e7655af379d0d73
Víctor Cançado <victor.cancado@gmail.com>
2020-12-02 01:41:21 -0300

    Req 7 adicionado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,15 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = () => {
+  // const student = {
+  //   name: ${nameP},
+  //   feedback: () => {
+  //     return 'Eita pessoa boa!';
+  //     },
+  //   };
+  //   return student;
+};
+createStudent('Leandrão, o Lobo Solitário');
 
 module.exports = createStudent;
