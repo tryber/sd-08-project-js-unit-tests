12b712cd3094beaba1238a3f8ee03634835a9391
mateusvictor7 <mateussantiago.victor@gmail.com>
2020-12-02 18:47:23 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -19,9 +19,9 @@
 const vqv = (nome, idade) => {
   if (nome && idade) {
     return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+    Tenho ${idade} anos,
+    trabalho na Trybe e mando muito em programação!
+    #VQV!`;
   }
   return undefined;
 };
