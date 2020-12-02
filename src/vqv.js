cf22d63f478696ec82016e6962d3dbd5832ebea8
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-12-02 10:36:56 -0300

    FIx template literal (espaços)

--- src/vqv.js
+++ src/vqv.js
@@ -20,9 +20,9 @@ const vqv = (nome, idade) => {
   if (nome == null && idade == null) {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!\n +
-    Tenho ${idade} anos,\n +
-    trabalho na Trybe e mando muito em programação!\n +
-    #VQV!`;
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 module.exports = vqv;
