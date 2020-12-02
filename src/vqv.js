efcab0a4410c557de00b5d73761e807892223e12
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-12-01 23:12:07 -0300

    Fix single quotes

--- src/vqv.js
+++ src/vqv.js
@@ -19,14 +19,10 @@
 const vqv = (nome, idade) => {
   if (nome == null && idade == null) {
     return undefined;
-  } else {
-    return (
-      `Oi, meu nome é ${nome}!\n` +
-      `Tenho ${idade} anos,\n` +
-      `trabalho na Trybe e mando muito em programação!\n` +
-      `#VQV!`
-    );
   }
+  return `Oi, meu nome é ${nome}!\n +
+    Tenho ${idade} anos,\n +
+    trabalho na Trybe e mando muito em programação!\n +
+    #VQV!`;
 };
-
 module.exports = vqv;
