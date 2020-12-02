5d7f8ba79522d385e899273410242e020fbb510e
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-12-01 23:02:34 -0300

    Tarefa 3 - vqv()

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,17 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome == null && idade == null) {
+    return undefined;
+  } else {
+    return (
+      `Oi, meu nome é ${nome}!\n` +
+      `Tenho ${idade} anos,\n` +
+      `trabalho na Trybe e mando muito em programação!\n` +
+      `#VQV!`
+    );
+  }
+};
 
 module.exports = vqv;
