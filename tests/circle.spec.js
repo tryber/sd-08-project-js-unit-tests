3ab9a3d80f03fb80d89a3064e05724e21a3cbfee
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-12-02 10:56:15 -0300

    Fix terceiro teste

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -32,7 +32,7 @@ describe('#circle', () => {
     // Teste se o objeto retornado tem 3 entradas.
     assert.strictEqual(arguments.length === 3, arguments.length === 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    assert.strictEqual(circle(), undefined);
+    assert.strictEqual(typeof circle(), 'undefined');
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
