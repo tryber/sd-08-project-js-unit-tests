95e652147df6530cd519a5d32b20508e1cecd9a7
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2020-12-02 19:37:34 -0300

    realizado requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const phrase = () =>{"Eita pessoa boa!"}
+  return {
+    name: name,
+    feedback: phrase
+  }
+};
+
+console.log(createStudent('thamy'));
 
 module.exports = createStudent;
