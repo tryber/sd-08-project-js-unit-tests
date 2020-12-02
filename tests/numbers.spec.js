cbdd8c054ceeb36a242e178c958dd63b01fd4b25
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2020-12-01 21:17:58 -0300

    correçao requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -24,6 +24,10 @@ describe('#numbers', () => {
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
     // Escreva um teste em que a função recebe [' '] e retorna false
-    assert.strictEqual(typeof(numbers), 'number');
+    if (typeof (numbers) === 'number') {
+      return true;
+    } else {
+      return false;
+    }
   });
 });
