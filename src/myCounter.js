0f42092891c0f77a382d3455c9dfa1d44ea91cde
Antônio Schappo <antonioschappo@gmail.com>
2020-12-02 20:36:34 -0300

    first try req. 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,13 +16,7 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-    }
-  }
+  var myArray = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
   return myArray;
 };
 
