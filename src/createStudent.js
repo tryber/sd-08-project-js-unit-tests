f15fed4e7108c160e8656026e3947a0e466e8a8b
Antônio Schappo <antonioschappo@gmail.com>
2020-12-01 23:20:34 -0300

    code climate fix(2) first try req. 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,9 +18,9 @@
 const createStudent = (nome) => {
   const object = {
     name: nome,
-    feedback: () => {'Eita pessoa boa!'}
-  }
-  return object
+    feedback: () => { 'Eita pessoa boa!' },
+  };
+  return object;
 };
 
 module.exports = createStudent;
