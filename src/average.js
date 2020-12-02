dd70603fceed0cd839455f8d0dba99cf34292d52
Víctor Cançado <victor.cancado@gmail.com>
2020-12-02 13:00:07 -0300

    Atualiza req 1: average

--- src/average.js
+++ src/average.js
@@ -17,9 +17,11 @@ const average = () => {
   if (numbers === '' || numbers !== 'number') {
     return undefined;
   }
-  let averageN = [];
+  let sumNumbers = 0;
+  let averageN = 0;
   for (let index = 0; index < numbers.length; index += 1) {
-    averageN = (numbers[index] + numbers[index]) / 2;
+    sumNumbers += numbers[index];
+    averageN = sumNumbers / numbers.length;
   }
   return Math.round(averageN);
 };
