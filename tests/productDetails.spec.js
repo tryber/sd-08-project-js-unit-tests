cd707c92272c237a296affc55f1aec6d262a2967
UmberBn <humberto-bandeira@hotmail.com>
2020-12-03 13:16:10 -0400

    corrigindo o bug do requisito 6, sem querer estava atribuindo uma nova função para o assert

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -39,21 +39,20 @@ describe('#productDetails', () => {
     const produto = productDetails('Alcool gel', 'Mascara');
     assert.strictEqual(Array.isArray(produto), true);
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.strictEqual = (produto.length, 2);
+    assert.strictEqual(produto.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-   /* assert.deepStrictEqual(typeof(produto[0]), 'object' );
-    assert.deepStrictEqual(typeof(produto[1]), 'object' );
+    assert.deepStrictEqual(typeof (produto), 'object');
     // Teste que os dois objetos são diferentes entre si.
     assert.notStrictEqual(produto[0], produto[1]);
-   /* // (Difícil) Teste que os dois productIds terminam com 123.
+    // (Difícil) Teste que os dois productIds terminam com 123.
     const finalWords = expected[0].details.productId;
     const finalWordsTwo = expected[1].details.productId;
     const resultFirst = finalWords.slice(-3);
     const resultSecond = finalWordsTwo.slice(-3);
     let finalResult = false;
-    if(resultFirst === "123" && resultSecond === "123"){
+    if (resultFirst === "123" && resultSecond === "123") {
       finalResult = true;
     }
-    assert.deepStrictEqual( finalResult, true);*/
+    assert.deepStrictEqual(finalResult, true);
   });
 });
