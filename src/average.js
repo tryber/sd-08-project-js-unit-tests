bec20e82b4db4ca08b380e76e79f9f24a7d16738
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2020-12-03 03:00:41 -0300

    Requisito 1, tentativa 2, code climate

--- src/average.js
+++ src/average.js
@@ -14,13 +14,13 @@
 
 const average = (array) => {
   let soma = 0;
-  if (array.length == 0) {
+  if (array.length === 0) {
     return undefined;
-  } else {
-    for (let index in array) {
-      soma += array[index];
-    }
   }
+  for (const index = 0; index < array.length; index++) {
+    soma += array[index];
+  }
+
   return Math.round(soma / array.length);
 };
 
