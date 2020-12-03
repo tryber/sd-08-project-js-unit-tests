a44f035d154615bba0dc834320e61f3cdf47648f
mateusvictor7 <mateussantiago.victor@gmail.com>
2020-12-02 21:40:47 -0300

    Requisito 4 correçao de erros

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -38,10 +38,10 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(onlyTwoDecimals(circle(2).circumference), 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(onlyTwoDecimals(circle(3).area), 28.56);
+    assert.strictEqual(onlyTwoDecimals(circle(3).area), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.strictEqual(circle(3).radius, 3);
-    assert.strictEqual(onlyTwoDecimals(circle(3).area), 28.56);
-    assert.strictEqual(onlyTwoDecimals(cicle(3).circumference), 18.84);
+    assert.strictEqual(onlyTwoDecimals(circle(3).area), 28.26);
+    assert.strictEqual(onlyTwoDecimals(circle(3).circumference), 18.84);
   });
 });
