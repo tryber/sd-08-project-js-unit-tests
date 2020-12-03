e7df04fd93169836088bb10c38aa342469a20ddc
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2020-12-03 01:44:03 -0300

    correçao requisito 8 - correçao code -climate

--- src/myCounter.js
+++ src/myCounter.js
@@ -26,12 +26,12 @@ const myCounter = () => {
       myArray.push(counter);
     }
   }
-  for (var counter = 2; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    myArray.push(counter);
+  for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
+    myArray.push(counter1);
+    myArray.push(counter1);
   }
-  for (var counter = 1; counter <= 3; counter += 1) {
-    myArray.push(counter);
+  for (let counter2 = 1; counter2 <= 3; counter2 += 1) {
+    myArray.push(counter2);
   }
   return myArray;
 };
