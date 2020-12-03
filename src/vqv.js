560b93cd158100e3d30daf6a49141c6e172edb08
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2020-12-03 03:48:13 -0300

    Requisito 3, tentativa 4, code climate

--- src/vqv.js
+++ src/vqv.js
@@ -18,14 +18,12 @@
 
 const vqv = (nome, idade) => {
   if (nome === undefined && idade === undefined) {
-    return undefined
+    return undefined;
   }
   return `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
-
-  return undefined;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
