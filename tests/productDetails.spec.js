222590ce898fce32d422176d2a83175b0104c76d
UmberBn <humberto-bandeira@hotmail.com>
2020-12-03 12:31:20 -0400

    recoloquei o fail no requisito 6 para ver o efeito nos outros testes.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,6 +33,7 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
+    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
     let expected = productDetails('mascara', 'alcool');
