8efc243cbaae2b49e1d16ca9d740c25aa3ce7412
Miguel Dantas <miiguelalb@gmail.com>
2020-12-03 00:02:51 -0300

    Average concluído

--- src/numbers.js
+++ src/numbers.js
@@ -8,6 +8,7 @@
     - numbers([2, 3, 4]); // Retorna: true
     - numbers([2, 'errado', 5]); // Retorna: false
 */
+// const assert = require('assert');
 
 const numbers = (myArray) => {
   for (let i = 0; i < myArray.length; i += 1) {
