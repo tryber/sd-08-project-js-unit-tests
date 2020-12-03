68e52ddd38b1a1ee9cd9e424d0ca4c9c9c7cb3c8
UmberBn <humberto-bandeira@hotmail.com>
2020-12-03 00:38:02 -0400

    average function

--- src/average.js
+++ src/average.js
@@ -12,6 +12,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length <= 0) {
+    return "undefined"
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) != "number") {
+      return "undefined"
+    };
+  };
+  let summation = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    summation += array[index];
+  };
+  const avg = Math.round(summation / array.length);
+  return avg;
+};
 
+console.log(average([9,9]))
 module.exports = average;
