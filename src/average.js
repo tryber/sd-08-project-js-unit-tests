ec234b6d850d782494d65e046e02f6e68d0a8e4d
UmberBn <humberto-bandeira@hotmail.com>
2020-12-03 00:50:32 -0400

    corrigindo erros codeClimate, ponto e virgula

--- src/average.js
+++ src/average.js
@@ -15,7 +15,7 @@
 const average = (array) => {
   if (array.length <= 0) {
     return 'undefined';
-  };
+  }
   for (let index = 0; index < array.length; index += 1) {
     if (typeof (array[index]) !== 'number') {
       return 'undefined';
