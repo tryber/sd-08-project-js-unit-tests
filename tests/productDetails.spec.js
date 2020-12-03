391f647ead4ce4ce534cbb9fd1fabf42ecae4e55
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2020-12-03 11:09:36 -0400

    Arrumando product detail

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -35,7 +35,7 @@ describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(Array.isArray(productDetails('valor1', 'valor2'), true));
+    assert.strictEqual(Array.isArray(productDetails('valor1', 'valor2')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails('valor1', 'valor2').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
