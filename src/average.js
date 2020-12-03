4ffb5a9900a66e53e0e5503202d432002bad5b6a
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2020-12-03 03:09:12 -0300

    Requisito 1, tentativa 3, avaliação do tipo do componente do array

--- src/average.js
+++ src/average.js
@@ -18,6 +18,8 @@ const average = (array) => {
     return undefined;
   }
   for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index] !== 'number'))
+      return undefined;
     soma += array[index];
   }
 
