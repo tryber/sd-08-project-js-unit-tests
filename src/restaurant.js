e5ad8da896a9bd405eec91a767db4790673cb987
Lucas Neiva <lucassneiva.code@gmail.com>
2020-12-04 18:58:30 -0300

    final commit

--- src/restaurant.js
+++ src/restaurant.js
@@ -68,7 +68,6 @@
 
 //------------------------------------------------------------------------------------------
 
-<<<<<<< HEAD
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 const orderFromMenu = (restaurante, str) => {
   restaurante.consumption.push(str);
@@ -98,10 +97,4 @@ const createMenu = (menu) => {
 
   return restaurante;
 };
-=======
-// PASSO 4: Adicione ao objeto `restaurant`, que foi retornado pela função `createMenu()` uma chave `pay` com uma função que itera por todos os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará iterar tanto pelo objeto da chave `food` quanto pelo objeto da chave `drink`.
-
-const createMenu = () => { };
-
->>>>>>> origin/master
 module.exports = createMenu;
