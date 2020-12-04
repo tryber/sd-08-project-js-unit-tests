b3d38bca4e4e9e435781d52d8b9352e04ab91c67
William Vieira <william.marangon@gmail.com>
2020-12-04 09:18:32 -0300

    exercicios de teste

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,17 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    const testArray = productDetails('Alcool gel', 'Máscara')
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(testArray), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(testArray.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof(testArray[0] && testArray[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual((testArray[0], testArray[1]), true)
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(testArray[0].details.productId.endsWith('123') === testArray[1].details.productId.endsWith('123'), true);
   });
 });
