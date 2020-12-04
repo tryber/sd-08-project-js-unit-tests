ff7895d432958e33a0fd3e42adc6b9c08dd135f3
Lucas Neiva <lucassneiva.code@gmail.com>
2020-12-04 18:55:43 -0300

    resolve conflitos merge

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -53,7 +53,6 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-<<<<<<< HEAD
     const menuTest3 = {
       food: { coxinha: 3.9, sanduiche: 9.9 },
       drinks: { agua: 3.9, cerveja: 6.9 },
@@ -117,10 +116,6 @@ describe('#createMenu', () => {
 
     assert.strictEqual(objetoRetornadoTeste8.pay(), 23.76);
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
-=======
-    assert.fail();
-    // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto que contêm a chave `fetchMenu` e esta por sua vez tem como valor uma função que ao ser executada retorna um objeto qualquer. Exemplo de retorno: { fetchMenu: function }.
->>>>>>> origin/master
     // ```
     // const objetoRetornadoTeste1 = createMenu(objetoQualquer) // Retorno: { fetchMenu: function }
     // const objetoRetornadoTeste1.fetchMenu() // retorno: objetoQualquer
