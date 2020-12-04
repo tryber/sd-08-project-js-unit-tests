26ee6d2254297af5f3108f33f5506446dffc7833
siebenei5 <siebenei7@gmail.com>
2020-12-04 14:12:21 +0000

    LAst

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -37,15 +37,6 @@ const createMenu = require('../src/restaurant');
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
 
-    const abc = createMenu('abc');
-    assert.strictEqual(typeof createMenu('abc'), 'object');
-    assert.strictEqual(abc, )
-
-
-
-
-
-
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto que contêm a chave `fetchMenu` e esta por sua vez tem como valor uma função que ao ser executada retorna um objeto qualquer. Exemplo de retorno: { fetchMenu: function }.
     // ```
     // const objetoRetornadoTeste1 = createMenu(objetoQualquer) // Retorno: { fetchMenu: function }
