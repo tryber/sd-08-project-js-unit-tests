83505a02448b3081e58237f3d25dd29009a51e71
Bruno <brunocmoraes97@gmail.com>
2020-12-04 17:16:05 -0300

    Correções code climate

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,11 @@ const vqv = (nome, idade) => {
   if (nome === '' || idade === '') {
     return undefined;
   }
-    return `Oi, meu nome é ${nome}!
+  return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
 };
 
+console.log(vqv());
 module.exports = vqv;
