39b360d8a5bc8f7ecfbf324f1d70cef9f5d930f1
Bruno <brunocmoraes97@gmail.com>
2020-12-04 08:28:32 -0300

    Adicionando verificação por tipo

--- src/average.js
+++ src/average.js
@@ -17,11 +17,15 @@ const assert = require('assert');
 const average = (array) => {
   let soma = 0;
   for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] == 'string' || array[index] === 0){
+      return undefined;
+    }
     soma += array[index];
   }
   return Math.round(soma / array.length);
 };
 
-assert.strictEqual(average([1, 2, 3, '4', 5]), undefined, 'undefined');
-assert.strictEqual(average([3, 4, 5]), 4);
+console.log(average([1, 0, 3]))
+//assert.strictEqual(average([1, 2, '3', 4, 5]), undefined);
+//assert.strictEqual(average([3, 4, 5]), 4);
 module.exports = average;
