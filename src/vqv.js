1903d2e47bbcbf6c59c7e74865b5964f43bd6e7f
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2020-12-04 13:17:00 -0300

    Passing all requirements needed of the project

--- src/vqv.js
+++ src/vqv.js
@@ -18,10 +18,7 @@
 
 const vqv = (nome, idade) => {
   if (nome && idade) {
-    return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação! 
-  #VQV!`;
+    return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
   }
   return undefined;
 };
