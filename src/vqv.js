76e8dab0451e70de55bf2ce58c99465fe541909b
vicoliveir <victorhugoqxd@gmail.com>
2020-12-04 11:23:39 -0300

    refactoring requiriment 6

--- src/vqv.js
+++ src/vqv.js
@@ -18,7 +18,7 @@
 
 
 const vqv = (nome, idade) => {
-  if (!nome || !idade) {
+  if (typeof nome !== 'string' || typeof idade !== 'number') {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!
