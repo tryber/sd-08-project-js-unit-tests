892987f08c89c16c512707d5ffa9a32a026cb50a
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2020-12-06 16:16:55 -0300

    mult instead of mul

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -22,7 +22,7 @@
 const calculator = {
   add: (a, b) => Math.floor(a + b),
   sub: (a, b) => Math.floor(a - b),
-  mul: (a, b) => Math.floor(a * b),
+  mult: (a, b) => Math.floor(a * b),
   div: (a, b) => Math.floor(a / b),
 };
 
