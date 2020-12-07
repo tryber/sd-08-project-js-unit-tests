3c846a262a13ca3528809206eb8a06c32593151d
Bruno <brunocmoraes97@gmail.com>
2020-12-07 15:29:12 -0300

    Code climate

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,7 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-const message = () => { console.log('Eita pessoa boa!') };
+const message = () => { console.log('Eita pessoa boa!'); };
 
 const createStudent = (name) => {
   const informations = {
