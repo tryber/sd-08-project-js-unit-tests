0f603e7524e2c2fee06823d771ff28f6ecd3bdfb
Samuel <samuelvanguarda@gmail.com>
2020-12-08 15:44:17 -0300

    Requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
+const { strict } = require('assert');
 const assert = require('assert');
 const numbers = require('../src/numbers');
 
@@ -20,8 +21,12 @@ describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
     assert.fail();
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    assert.strictEqual(numbers([1, 2, 3, 4, 5]), true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    assert.strictEqual(numbers([1, 'a', 3]), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    assert.strictEqual(numbers([' ']), false);
   });
 });
