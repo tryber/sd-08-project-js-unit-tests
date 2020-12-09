9ff708a11761eaf7761af47c934d9f4389347928
Samuel <samuelvanguarda@gmail.com>
2020-12-09 09:30:15 -0300

    Requisito 4 - Correção

--- src/vqv.js
+++ src/vqv.js
@@ -20,7 +20,7 @@ const vqv = (nome, idade) => {
   if (typeof nome !== 'string' || typeof idade !== 'number') {
     return undefined;
   }
-  const message = `Oi, meu nome é ${nome}!
+  const message =  `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
