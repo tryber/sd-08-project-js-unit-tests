14c96eeb862fd273161c8b2070833f7d058cf2d4
Samuel <samuelvanguarda@gmail.com>
2020-12-09 10:15:44 -0300

    Requisito 5 - Correção 2

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,9 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
+const createStudent = (firstName) => {
   const student = {
-    nome: name,
+    name: firstName,
     feedback: () => 'Eita pessoa boa!',
   };
   return student;
