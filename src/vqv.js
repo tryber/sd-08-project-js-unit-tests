0b62c53e1ebef340573882f683c40d2d257279a3
Samuel <samuelvanguarda@gmail.com>
2020-12-09 09:33:00 -0300

    Requisito 4 - Code Climate

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,10 @@ const vqv = (nome, idade) => {
   if (typeof nome !== 'string' || typeof idade !== 'number') {
     return undefined;
   }
-  const message =  `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  const message = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
   return message;
 };
 module.exports = vqv;
