942bc4881fbf30f7513d4b8a82faf99eaee2f761
Lucas Duque <lucas3rios2014@gmail.com>
2020-12-09 20:24:59 -0300

    Comsertando codeclimate

--- src/average.js
+++ src/average.js
@@ -14,7 +14,7 @@
 const average = (array) => {
   let sun = 0;
   if (array.length === 0) return undefined;
-  for (let index = 0; index < array.length; index +=1) {
+  for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] === 'number') {
       sun += array[index];
     } else return undefined;
