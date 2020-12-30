a483fb4104b4052128194c93ed4ca16cea38a165
Bruno <brunocmoraes97@gmail.com>
2020-12-29 21:59:37 -0300

    corrige requisito 8 que estava retornando uma string

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -151,7 +151,7 @@ describe('#createMenu', () => {
        objetoRetornadoTeste8.order("agua")
        objetoRetornadoTeste8.order("coxinha")
        objetoRetornadoTeste8.order("agua")
-       assert.deepStrictEqual(objetoRetornadoTeste8.pay(), 12.87)
+       assert.strictEqual(Number(objetoRetornadoTeste8.pay()), 12.87)
     // Agora faça o PASSO 4 no arquivo `src/restaurant.js`.
   });
 });
