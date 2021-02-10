fa407a66bb0ecb4ddab318ff80103da1dbe19c24
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-02-10 16:46:59 -0300

    requisito 01

--- src/average.js
+++ src/average.js
@@ -11,6 +11,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let soma = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    soma += array[i];
+  }
+  return Math.round(soma / array.length);
+};
 
 module.exports = average;
